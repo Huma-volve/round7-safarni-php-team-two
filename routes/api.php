9640a0c2924f7226db623455b58ff2b285f03d70
Ahmed abdeldaym <ahmedabdeldaem01000@gmail.com>
2025-11-16 21:25:38 +0200

    in the begin i added  migrations,model,factory,seeders and controller for 2 api 1:get all hotels 2:show hotel with rooms 3:hotel nearby

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\HotelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::prefix('hotels')->group(function () {
+    Route::get('/', [HotelController::class, 'index']);
+    Route::get('/{id}', [HotelController::class, 'show']);
+    Route::get('/{id}/nearby', [HotelController::class, 'nearby']);
+});
