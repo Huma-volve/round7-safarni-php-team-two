a352269bd1074ace076d0f3ac0a0b0cf8435d822
yasmina223 <yasminaelfarahty@gmail.com>
2025-11-18 19:32:59 +0200

    auth api

--- routes/api.php
+++ routes/api.php
@@ -23,15 +23,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [LoginController::class, 'logout']);
      Route::get('profile', [ProfileController::class, 'profile']);
     Route::post('update/profile', [ProfileController::class, 'updateProfile']);
-});
-
-   
-
-// Route::middleware('auth:sanctum')->group(function () {
-
-
-
-    /*Hotels*/
 
     Route::get('hotels', [HotelController::class, 'index']);/*✅ all  hotels*/
 
@@ -41,22 +32,24 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('hotels-nearby', [HotelController::class, 'nearby']);    /* ✅ nearby  hotels */
 
-    Route::post('hotels-actions', [HotelController::class, 'store']); // ✅ add new hotel     
+    Route::post('hotels-actions', [HotelController::class, 'store']); // ✅ add new hotel
 
-    Route::put('hotels-actions/{hotel}', [HotelController::class, 'update']); // ✅ update hotel  
+    Route::put('hotels-actions/{hotel}', [HotelController::class, 'update']); // ✅ update hotel
 
     Route::delete('hotels-actions/{hotel}', [HotelController::class, 'destroy']);// ✅ delete hotel
     /*Rooms*/
 
-    Route::post('hotels/{hotel}/rooms', [RoomController::class, 'store']); // ✅ add new Room   
+    Route::post('hotels/{hotel}/rooms', [RoomController::class, 'store']); // ✅ add new Room
 
-    Route::put('hotels/{hotel}/rooms/{room}', [RoomController::class, 'update']);// ✅  update Room   
+    Route::put('hotels/{hotel}/rooms/{room}', [RoomController::class, 'update']);// ✅  update Room
 
     Route::get('hotels/{hotel}/rooms/{room}', [RoomController::class, 'show']);// ✅  show Room
 
     Route::delete('hotels/{hotel}/rooms/{room}', [RoomController::class, 'destroy']);// ✅  delete Room
 
-    Route::get('hotels/{hotel}/rooms', [RoomController::class, 'index']);// ✅  All Rooms in the Hotel   
+    Route::get('hotels/{hotel}/rooms', [RoomController::class, 'index']);// ✅  All Rooms in the Hotel
+});
 
 
-// });
+
+
